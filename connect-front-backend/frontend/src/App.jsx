e8277a98cf1148824b4f3ef8eccc3472f56f1f82
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-02 17:09:30 +0530

    data handled function will be created

--- connect-front-backend/frontend/src/App.jsx
+++ connect-front-backend/frontend/src/App.jsx
@@ -6,9 +6,16 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Jokes</h1>
+      <p>Count of Jokes : {jokes.length}</p>
+      {jokes.map( (jokes, index) => {
+        <div key={jokes.id}>
+          <h1>{jokes.title}</h1>
+          <h3>{jokes.body}</h3>
+        </div>
+      })}
     </>
   )
-}
+} 
 
 export default App
