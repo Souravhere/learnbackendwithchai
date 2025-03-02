401e8a083ed80079a5c11308330bde968c9a6da0
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-02 19:30:51 +0530

    big dev dep will be added

--- connect-front-backend/frontend/src/App.jsx
+++ connect-front-backend/frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+
 
 function App() {
   const [jokes, setjojkes] = useState([])
-
+  useEffect(() => {
+    fetch('http://localhost:4000/jokes')
+    .then(response => setjojkes(response))
+    
+  })
   return (
     <>
       <h1>Jokes</h1>
       <p>Count of Jokes : {jokes.length}</p>
       {jokes.map( (jokes, index) => {
         <div key={jokes.id}>
-          <h1>{jokes.title}</h1>
+          <h1>{setjojkes.title}</h1>
           <h3>{jokes.body}</h3>
         </div>
       })}
