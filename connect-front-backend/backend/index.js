ac8081a41eb7f53b17863c826223ce5a8a6ead0e
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-02 16:37:35 +0530

    a excption handler will be added

--- connect-front-backend/backend/index.js
+++ connect-front-backend/backend/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = 4000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
+const port = process.env.PORT || 4000;
 
 app.listen( port , () => {
     console.log(`Server running at http://localhost:${port}`);
