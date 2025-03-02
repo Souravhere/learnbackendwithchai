5bcf03bdd898059905439545c1eaf27eee2b7827
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-02 17:02:16 +0530

    react app will be created

--- connect-front-backend/backend/index.js
+++ connect-front-backend/backend/index.js
@@ -6,6 +6,37 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.get("/jokes", (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "Joke 1",
+            body: "This is a joke one for you"
+        },
+        {
+            id: 2,
+            title: "Joke 2",
+            body: "This is a joke two for you"
+        },
+        {
+            id: 3,
+            title: "Joke 3",
+            body: "This is a joke three for you"
+        },
+        {
+            id: 4,
+            title: "Joke 4",
+            body: "This is a joke four for you"
+        },
+        {
+            id: 5,
+            title: "Joke 5",
+            body: "This is a joke five for you"
+        },
+    ]
+    res.send(jokes);
+});
+
 const port = process.env.PORT || 4000;
 
 app.listen( port , () => {
