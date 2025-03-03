1607e364909183027df8b52e9cbbea8e5f6cef89
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-03 19:40:42 +0530

    todo model will be completed

--- createmodels/models/todos/todos.models.js
+++ createmodels/models/todos/todos.models.js
@@ -2,7 +2,24 @@ import mongoose from 'mongoose';
 
 const todosSchema = new mongoose.Schema(
     {
-
+        content:{
+            type: String,
+            required: true,
+        },
+        completed:{
+            type: Boolean,
+            default: false,
+        },
+        createdby: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        subtodos: [ // here by defaule we will created arry
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref:'SubTodo '
+            } //here we will added the subtodo 
+        ]
     },
     {timestamps:true}
 )
