e886b31979651535e02b26148fc87a6880ee913a
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-03 19:44:40 +0530

    sub todo final modeling will be completed

--- createmodels/models/todos/sub_todos.models.js
+++ createmodels/models/todos/sub_todos.models.js
@@ -1,7 +1,20 @@
 import mongoose  from "mongoose";
 
 const subTodosSchema = new mongoose.Schema(
-    {},
+    {
+        title:{
+            type: String,
+            required: true
+        },
+        completed:{
+            type: Boolean,
+            default: false
+        },
+        createdby:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'User'
+        }
+    },
     {timestamps:true}
 );
 
