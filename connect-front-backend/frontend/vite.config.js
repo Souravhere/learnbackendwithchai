94bd05ea702e3f7e0be4248a3c11f3c7ad0478b0
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-03 18:19:31 +0530

    bit comment will be added

--- connect-front-backend/frontend/vite.config.js
+++ connect-front-backend/frontend/vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // here will will added the servier proxy
   server:{
+    // with this proxy we are telling vite to redirect all the request that start with /api to http://localhost:4000
     proxy:{
       '/api': "http://localhost:4000"
     },
