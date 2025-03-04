755788c6148dc129e8d4b5c8263bcc3ec9e65ade
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-04 17:06:48 +0530

    basic order model will be created

--- createmodels/models/ecommerce/orders.models.js
+++ createmodels/models/ecommerce/orders.models.js
@@ -1,7 +1,20 @@
 import mongoose from "mongoose";
 
 const orderSchema = mongoose.Schema(
-    {},
+    {
+        orderPrice:{
+            type: Number,
+            required: true
+        },
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required: true
+        },
+        orderItems:{
+            type: []
+        }
+    },
     {timestamps: true}
 );
 
