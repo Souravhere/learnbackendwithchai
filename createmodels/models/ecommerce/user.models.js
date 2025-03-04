09b59b6eabe8fc18778f575b526b0d2a8c22c5e0
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-03-04 16:35:55 +0530

    first user model some structure will be created

--- createmodels/models/ecommerce/user.models.js
+++ createmodels/models/ecommerce/user.models.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({},{timestamps:true});
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase:true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase:true
+        }
+    },
+    {timestamps:true});
 
 export const User = mongoose.model("User", userSchema);
